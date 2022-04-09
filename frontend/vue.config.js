f6074f8fb8a23f0da18083ae270e394bd83d6d23
Dmitrii Ivanov <idma88@yandex.ru>
2022-04-09 23:06:42 +0300

    [frontend] Fix `invalid host header` problem

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    devServer: {
-      watchOptions: {
-        ignored: ['**/public/**/*', '/node_modules'],
-      },
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3000/',
-          ws: true,
-          changeOrigin: true
-        }
+  devServer: {
+    disableHostCheck: true,
+    watchOptions: {
+      ignored: ['**/public/**/*', '/node_modules'],
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        ws: true,
+        changeOrigin: true
       }
     }
   }
+}
